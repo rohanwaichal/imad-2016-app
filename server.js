71767150dae18433f0eba2025b676823f636f057
rohanwaichal <rohanwaichal007@gmail.com>
2016-10-12 14:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one',function(req, res){
+   res.send('Ariical one is requsted..!'); 
+});
+
+app.get('/artical-two',function(req, res){
+   res.send('Ariical two is requsted..!'); 
+});
+
+app.get('/artical-three',function(req, res){
+   res.send('Ariical three is requsted..!'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
