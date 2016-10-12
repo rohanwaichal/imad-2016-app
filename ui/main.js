cf516727eab77dc5fe408f95f2aa44ab3b70856b
rohanwaichal <rohanwaichal007@gmail.com>
2016-10-12 19:09:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,7 @@
-console.log('Loaded!');
-
-//button counter
 var button = document.getElementById('counter');
 var counter = 0;
-button.onclick = function() {
-    
-    //rendering the variable in correct span
-    counter = counter + 1;
-    varspan = document.getElementById("count");
-    span.innerHTML = counter.toString();
-}
+button.onclick = function (){
+  counter = counter + 1;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toSting();
+};
