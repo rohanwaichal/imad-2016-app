dccf55533074a67f1fbe434018b75445f27eeff6
rohanwaichal <rohanwaichal007@gmail.com>
2016-10-12 18:04:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text')
 element.innerHTML = "New Value"
 
 //move the image
-var img = document.getElementById('abc')
-img.onclick = function() {
-    img.style.margineleft = '100px';    
-}
+var img = document.getElementById('madi')
+img.onclick = function(){
+  img.style.marginleft = '100px';  
+};
